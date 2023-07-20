af9822d96962546e7d27187eebd4a5fbe1097b23
cyrogabrielcc <cyrocunhacg@gmail.com>
2023-07-20 20:12:30 -0300

    first commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TrabalhandoComEF.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("horadata")]
+        public IActionResult GetDateHour()
+        {
+            var obj = new {
+                Data = DateTime.Now.ToLongDateString(),
+                Hora = DateTime.Now.ToShortTimeString()
+            };
+            return Ok(obj);
+        }
+    }
+}
